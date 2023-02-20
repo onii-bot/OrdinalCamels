ce277198da698ab090b9d50cbbf7d06dd234a8e5
onii <fabianmann9@gmail.com>
2023-02-20 21:45:55 +0545

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <Home />
   <About />
+  <Footer />
 </template>
 
 <script>
 
 import Home from './components/Home.vue'
 import About from './components/About.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
     Home,
-    About
+    About,
+    Footer
   }
 }
 </script>
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,22 +1,48 @@
 <template>
-<section class="section-about">
-  <div class="text-center mb-20">
-    <h2 class="heading-secondary">
-    About Ordinal Camels
-  </h2>
-  </div>
-  <div class="container-about text-center">
-    <img class="camel-photo mx-auto mb-32" src="../assets/camel-nft.png" alt="camel">
-    <h3 class="heading-tertiary mb-10">
-      Who are we?
-    </h3>
-    <p class="paragraph mx-[4%]">We are a brand new NFT collection of 125 pixelated Camels inscribed as ordinals on the BTC blockchain forever! Not a basic copy pasta or deriv NFT project from an existing ETH collection either. 🐪🐪🐪</p>
-  </div>
-</section>
+  <section class="section-about">
+    <div class="text-center mb-20">
+      <h2 class="heading-secondary">About Ordinal Camels</h2>
+    </div>
+    <div class="container-about text-center">
+      <img
+        class="camel-photo mx-auto mb-32"
+        src="../assets/camel-nft.png"
+        alt="camel"
+      />
+      <div class="mb-[10rem]" v-for="item in faq" :key="item.question">
+        <h3 class="heading-tertiary mb-5">
+          {{ item.question }}
+        </h3>
+        <p class="paragraph mx-[4%]">
+          {{ item.answer }}
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
   name: "my-about",
+  data() {
+    return {
+      faq: [
+        {
+          question: "Who are we?",
+          answer:
+            "We are a brand new NFT collection of 125 pixelated Camels inscribed as ordinals on the BTC blockchain forever! Not a basic copy pasta or deriv NFT project from an existing ETH collection either. 🐪🐪🐪",
+        },
+        {
+          question: "How many Camels are in the total collection?",
+          answer: "125 Camels",
+        },
+        {
+          question: "Why a collection on Bitcoin?",
+          answer:
+            "Ordinals are stored fully on-chain. Your Camel is permanently stored on the most secure blockchain.",
+        },
+      ],
+    };
+  },
 };
 </script>
